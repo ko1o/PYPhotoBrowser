14707c9635514c018b538ce7d7474ccc0dc3fdd1
iphone5solo <499491531@qq.com>
2017-11-03 13:20:51 +0800

    Updated SDWebImage to v4.2.1
    Updated SDWebImage to v4.2.1

--- Dependency/SDWebImage/SDWebImageCompat.h
+++ Dependency/SDWebImage/SDWebImageCompat.h
@@ -93,11 +93,11 @@
     #define SDDispatchQueueSetterSementics assign
 #endif
 
-extern UIImage *SDScaledImageForKey(NSString *key, UIImage *image);
+FOUNDATION_EXPORT UIImage *SDScaledImageForKey(NSString *key, UIImage *image);
 
-typedef void(^SDWebImageNoParamsBlock)();
+typedef void(^SDWebImageNoParamsBlock)(void);
 
-extern NSString *const SDWebImageErrorDomain;
+FOUNDATION_EXPORT NSString *const SDWebImageErrorDomain;
 
 #ifndef dispatch_main_async_safe
 #define dispatch_main_async_safe(block)\
@@ -107,5 +107,3 @@ extern NSString *const SDWebImageErrorDomain;
         dispatch_async(dispatch_get_main_queue(), block);\
     }
 #endif
-
-static int64_t kAsyncTestTimeout = 5;
